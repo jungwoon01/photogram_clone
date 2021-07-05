2a2356f323d631f9de734f3eb5ab37f0f2313124
jungwoon <jungwoon018@gmail.com>
2021-07-05 22:10:58 +0900

    User 모델 생성과 Auth 컨트롤러 수정

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,11 +1,16 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
+
+    Logger log = LoggerFactory.getLogger(AuthController.class);
 
     // 로그인 페이지
     @GetMapping("/auth/signin")
@@ -20,8 +25,9 @@ public class AuthController {
     }
 
     // 회원가입버튼 -> /auth/signup -> /auth/signin
-    @PostMapping("/auth/signup")
-    public String signup() {
+    @PostMapping("/auth/signup") // key = value (x-www-form-urlencoded)
+    public String signup(SignupDto signupDto) {
+        log.info(signupDto.toString());
         return "auth/signin";
     }
 }
